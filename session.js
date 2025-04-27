1fd41c57afda5bd6ec35ee28bda953b8784956ad
DarkeNima <naviyaoficial@gmail.com>
2025-04-27 12:10:45 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,10 +40,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
-    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~HE8jkaIY#Oh_R6iG8WlxikfJRtPk_8KrPyLz5Be9OHkg7VNQtRGs",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "A11tswb98XUEFX9jm2UOQwNs6tWaoB3Kq6Kn",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "DarkeNima",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94764038550", "94724173633"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
     FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
